85f0f49082a881f415253d7c82f13307202dbce1
Rich Stadnick <rich.stadnick@gmail.com>
2022-05-23 16:51:01 -0400

    Switched to hash router and added some info to about me.

---
+++ src/components/Project.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+interface IProjectProps {};
+
+const Project: FC<IProjectProps> = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+export default Project;
---
+++ src/pages/Blog.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import RootPage from "./root";
+import Container from "react-bootstrap/Container";
+
+const Blog: FC = () => {
+    return (
+        <RootPage>
+            <Container className="m-4 text-center">
+                <h2>I'm working on it...</h2>
+                </Container>
+        </RootPage>
+    );
+}
+export default Blog;
